6a5ea6c7dae9982dffe4ebc2622dbeb78337abec
RobertsonJoe <jwj.robert@gmail.com>
2016-08-02 22:51:25 -0400

    Detect CSS animation keyframes end

--- app/slider/image-slider.js
+++ app/slider/image-slider.js
@@ -1,9 +1,37 @@
 import './styles/main.styl';
 
 const imageSlider = document.getElementById('image-slider');
-
+const testAnimation = document.getElementById('test-animation');
 const click = document.getElementById('click');
+
 const images = [{}, {}, {}];
+
+
+function animationEnd() {
+    let i;
+	const temp = document.createElement('temp');
+
+    const animations = {
+        "animation": "animationend",
+        "OAnimation": "oAnimationEnd",
+        "MozAnimation": "animationend",
+        "WebkitAnimation": "webkitAnimationEnd"
+    };
+
+    for (i in animations) {
+    	console.log(temp.style[i]);
+    	if(temp.style[i] !== undefined) {
+    		console.log('TRUE');
+    		return animations[i];
+    	}
+    }
+}
+
+const endAnimation = animationEnd();
+console.log('test-animation', endAnimation);
+testAnimation.addEventListener('animationend', () => {
+	console.log('Animation Ended');
+});
 
 images.forEach((image, index) => {
     image.element = document.createElement('div');
